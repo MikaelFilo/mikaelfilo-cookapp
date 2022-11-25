7ae000222ce582db5a197af71c9b1b90cde0381f
MikaelFilo <mikial93@hotmail.com>
2022-11-25 18:48:02 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'a24ac6eb-b4ae-43e1-9ae5-384a2a47788f';
+export const MODAL_CLOSE_SEC = 2.5;
